f27cc9656e57bfd8992e5a30c093c8488a6cc592
wrrouman <106677200+wrrouman@users.noreply.github.com>
2023-01-26 22:52:09 -0500

    Heaader first draft & asset upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+// import { getStockPrice } from "./utils/apiCall";
+// import { useState, useEffect } from "react";
+import Header from "./components/Header/Header";
+import axios from "axios";
+import "./App.css";
+import { createContext, useState } from "react";
+import { ThemeContext } from "./components/Header/Header";
+// const api = "https://api.twelvedata.com/quote?symbol=spx&apikey=0f94898a0e174bf3ab5fe7600a1ba572"
 
-import './App.css';
+const apiKey = "0f94898a0e174bf3ab5fe7600a1ba572";
+const symbol = "SPY";
+const interval = "1day";
+
+const url = `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=${interval}&apikey=${apiKey}`;
 
 function App() {
   return (
-    <div className="App">
-    <h1>This is working Perfectly
-  
-    </h1>
-    </div>
+    <>
+      <Header />
+      <div>
+        <h1>This is working </h1>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,110 @@
+import "./Header.scss";
+import { React } from "react";
+import { useState, createContext } from "react";
+import { Link } from "react-router-dom";
+import ReactSwitch from "react-switch";
+import { TickerTape } from "react-ts-tradingview-widgets";
+
+const styles = {
+  symbols: [
+    {
+      proName: "FOREXCOM:SPXUSD",
+      title: "S&P 500",
+    },
+    {
+      description: "QQQ",
+      proName: "NASDAQ:QQQ",
+    },
+    {
+      description: "Meta",
+      proName: "NASDAQ:FB",
+    },
+    {
+      description: "Apple",
+      proName: "NASDAQ:AAPL",
+    },
+    {
+      description: "Amazon",
+      proName: "NASDAQ:AMZN",
+    },
+    {
+      description: "Netflix",
+      proName: "NASDAQ:NFLX",
+    },
+    {
+      description: "Google",
+      proName: "NASDAQ:GOOG",
+    },
+    {
+      description: "Tesla",
+      proName: "NASDAQ:TSLA",
+    },
+  ],
+  parent: {
+    fontSize: "0px",
+    lineHeight: "0px",
+    color: "red",
+  },
+  link: {
+    textDecoration: "line-trough",
+  },
+  span: {
+    color: "darkblue",
+  },
+};
+
+export const ThemeContext = createContext(null);
+
+// export const ThemeContext = createContext(null);
+function Header() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <section>
+          <div className="header" id={theme}>
+            <div className="header_logo-container">
+              <div className="header_logo">
+                <span className="header_green">Trade</span>
+                <span className="header_yellow">By</span>
+                <span className="header_red">Colors</span>
+              </div>
+              <div className="header_logo-subheader">
+                Highlight Market Momentum
+              </div>
+            </div>
+
+            <div className="header_hamburger">
+              <div className="header_watchlist">Watchlist</div>
+              <div className="header_about">About</div>
+              <div className="header_switch">
+                <label className="header_switch-label"></label>
+                <div className="header_switch">
+                  <label>
+                    {theme === "light" ? "Light Mode" : "Dark Mode"}
+                  </label>
+                  <ReactSwitch
+                    onChange={toggleTheme}
+                    checked={theme === "dark"}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <TickerTape
+            copyrightStyles={styles}
+            line-height="0"
+            symbols={styles.symbols}
+          />
+        </section>
+      </ThemeContext.Provider>
+    </>
+  );
+}
+
+export default Header;
