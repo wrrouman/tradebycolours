14853de8c0df1a064e498b8bccb801c696371a5e
wrrouman <106677200+wrrouman@users.noreply.github.com>
2023-03-04 09:11:42 -0500

    Design change

--- client/src/pages/Signup/index.jsx
+++ client/src/pages/Signup/index.jsx
@@ -12,9 +12,7 @@ function Signup() {
 		<div className={styles.container}>
 			<h1 className={styles.heading}>Sign up Form</h1>
 			<div className={styles.form_container}>
-				<div className={styles.left}>
-					<img className={styles.img} src="./images/signup.jpg" alt="signup" />
-				</div>
+				
 				<div className={styles.right}>
 					<h2 className={styles.from_heading}>Create Account</h2>
 					<input type="text" className={styles.input} placeholder="Username" />
